5d179b6a39f43c14e3095c329026a1269b2fbe1a
ashiwn <its.ashwin.23@gmail.com>
2025-10-16 21:44:01 +0530

    i have updated the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ const page = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <button>
-        <a href="/admin">Admin</a>
-      </button>
-      <button>
-        <a href="/login">Login</a>
-      </button>
+      
     </div>
   )
 }
